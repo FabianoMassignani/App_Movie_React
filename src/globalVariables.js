7990f009ed9a3427b3a340ede5e11c05238a3728
Fabiano <fabiano-smi@hotmail.com>
2022-04-20 22:53:49 -0300

    refs #11

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -1,17 +1,18 @@
 import NoImage from "./assets/image-not-found.png";
 
-// export const IMG_API = process.env.REACT_APP_IMG_API;
+export const HOST_API = "http://192.168.0.100:3001";
+
+//tmdb
 export const API_KEY = "e033694029aca8ecc22a82a3b615a643";
 export const LANGUAGE = "en-US";
- 
+
+export const IMG_API_LOW = "https://image.tmdb.org/t/p/w300";
+export const IMG_API_HIGH = "https://image.tmdb.org/t/p/w400";
+export const NO_IMAGE = NoImage;
+
+// track
 export const TRAKT_USERNAME = "FabianoM";
 export const CLIENT_ID =
   "75cd2c57aa26abf76745ef33aedf5cbcc8502cb95cb7d56f28ffe0b2d2c10f8e";
 export const CLIENT_SECRET =
   "1cc38f275b7e9cbb745c36767749cc08a08120a7c09b96799c23612ca56c6643";
-
-export const IMG_API_LOW = "https://image.tmdb.org/t/p/w300";
-export const IMG_API_HIGH = "https://image.tmdb.org/t/p/w400";
-export const HOST_API = "http://192.168.0.100:3001";
-
-export const NO_IMAGE = NoImage;
