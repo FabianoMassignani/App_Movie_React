b2b1d71fbe22659008a2ab930bc4c3929792aaf0
Fabiano Massignani Figueredo <40632931+FabianoMassignani@users.noreply.github.com>
2024-02-24 08:00:34 -0300

    Update globalVariables.js

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -4,7 +4,7 @@ export const HOST_API = "";
 
 //tmdb
 export const API_URL = "https://api.themoviedb.org/3";
-export const API_KEY = "e033694029aca8ecc22a82a3b615a643";
+export const API_KEY = "";
 export const LANGUAGE = "en-US";
 
 export const IMG_API_LOW = "https://image.tmdb.org/t/p/w342";
@@ -12,8 +12,8 @@ export const IMG_API_HIGH = "https://image.tmdb.org/t/p/w780";
 export const NO_IMAGE = NoImage;
 
 // track
-export const TRAKT_USERNAME = "FabianoM";
+export const TRAKT_USERNAME = "";
 export const CLIENT_ID =
-  "75cd2c57aa26abf76745ef33aedf5cbcc8502cb95cb7d56f28ffe0b2d2c10f8e";
+  "";
 export const CLIENT_SECRET =
-  "1cc38f275b7e9cbb745c36767749cc08a08120a7c09b96799c23612ca56c6643";
+  "";
